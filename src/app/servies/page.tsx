b958ce41ba4f53ef20e80eea66bd93443dac04b0
megatech2000 <151417898+megatech2000@users.noreply.github.com>
2024-05-19 11:54:37 +0530

    updating
    changes updated

--- src/app/servies/page.tsx
+++ src/app/servies/page.tsx
@@ -1,8 +1,18 @@
 const serviesPage = () => {
   return (
-    <div>
-      <div>servies page</div>
-    </div>
+    <section>
+      <div className="h-screen flex flex-col items-center justify-center font-extrabold ">
+        <h1 className="mb-0">Site Under Maintance!!</h1>
+        <p className="py-5">We Will be Back Soon!!!</p>
+        <div>
+          <a href="/">
+            <button className="px-10 py-2 rounded-full border border-[#2b2a2a] text-white hover:bg-white hover:text-black btnHover">
+              Return Home
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
   );
 };
 
